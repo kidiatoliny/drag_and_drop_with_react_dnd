6bf92b9e6da472f740af4717f226722fd7065ede
kid <kidiatoliny@gmail.com>
2020-10-22 00:24:19 -0100

    Create list component

--- src/pages/components/Board/index.tsx
+++ src/pages/components/Board/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import List from '../List';
 
+
  import { Container } from './styles';
 
 const Board: React.FC = () => {
   return (
     <Container>
+      <List/>
+      <List/>
+      <List/>
       <List/>
     </Container>
   );
--- src/pages/components/List/index.tsx
+++ src/pages/components/List/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
  import { Container } from './styles';
-
+ import { MdAdd } from 'react-icons/md'
 const List: React.FC = () => {
-  return (
+  return(
     <Container>
-      List
+      <header>
+        <h2>Tarefa</h2>
+      </header>
+      <button type="button">
+        <MdAdd size={24} color="#fff"/>
+      </button>
     </Container>
   );
 }
